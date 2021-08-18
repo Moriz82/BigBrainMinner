39688b8e0635b6f5ed5f94b88171e90574434127
Moriz82 <72836413+Moriz82@users.noreply.github.com>
2021-08-17 23:48:04 -0500

    a

--- src/Main.lua
+++ src/Main.lua
@@ -1,4 +1,8 @@
 --https://tweaked.cc/module/turtle.html
+textutils.slowPrint("Make sure fuel is in slot 1, and chests are in slot 2")
+textutils.slowPrint("Enter 2d x and y values")
+inputX = read("Enter x value: ")
+inputY = read("Enter y value: ")
 
 textutils.slowPrint("It do be digging tho")
 
@@ -25,17 +29,33 @@ function moveForward()
     end
 end
 
-for j = 1, 40 do
+function emptyInventory()
+    moveForward()
+    turtle.back()
+    turtle.select(2)
+    if not turtle.getItemCount() < 1 then
+        turtle.place()
+        for i = 3, 14 do
+            turtle.select(i)
+            turtle.drop()
+        end
+    end
+
+end
+
+for j = 1, inputX do
     moveForward()
 end
 
-for i = 1, 39 do
+emptyInventory()
+
+for i = 1, inputY do
 
     turtle.turnRight()
     moveForward()
     turtle.turnRight()
 
-    for j = 1, 40 do
+    for j = 1, inputX do
         moveForward()
     end
 
@@ -43,7 +63,9 @@ for i = 1, 39 do
     moveForward()
     turtle.turnLeft()
 
-    for j = 1, 40 do
+    for j = 1, inputX do
         moveForward()
     end
+
+    emptyInventory()
 end
